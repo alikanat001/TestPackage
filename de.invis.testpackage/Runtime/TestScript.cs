669b7a8ca67df4284307563682c1732db0f8cf9b
ali <ali.kanat@inpro.de>
2022-11-30 12:42:38 +0100

    fasf

---
+++ de.invis.testpackage/Runtime/TestScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace invis.testpackage
+{ 
+    public class TestScript
+    {
+       public void DisplayTestMessage()
+        {
+            print("Test package method is called");
+        }
+    }
+}
